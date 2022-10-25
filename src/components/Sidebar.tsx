6727079361568a34a968cbdbcca1b1b34304a715
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2022-10-25 19:08:47 -0300

    Criando Tipagens

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar(){
+    return(
+        <aside className={styles.sidebar}>
+            <img  className={styles.cover} src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
+
+            <div className={styles.profile}>
+           <Avatar src="https://github.com/GabrielMatoss.png"/>
+
+            <strong>Gabriel Matos</strong>
+            <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
